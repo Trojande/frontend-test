c3638c1502c2314dfe15789a81c791cecf73ca68
trojande <trojande@artur-debian>
2017-05-31 05:15:23 +0700

    one element view

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,8 @@ app.config(function($httpProvider, $locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('');
     $routeProvider
     .when('/book/:postId', {
-        template: '<h1> this is my cool post </h1>'
+        templateUrl: 'views/book.html',
+        controller: 'BookCtrl'
     })
     .otherwise({
         templateUrl: 'views/list.html' ,
@@ -33,24 +34,45 @@ app.service('searchService', function($http) {
         return response.data;
       });
       return promise;
+    },
+    getBookInfo: function(searchInput) {
+        const req = {
+            method: "GET",
+            url: "http://localhost:8080/api/v1/items/item_details/"+searchInput,
+            headers: {
+                'Authorization' : "Token token="+this.token,
+                'Content-type' : 'application/json; charset=utf8'
+            },
+            withCredentials: true
+        }
+            let promise = $http(req).then(function (response) {
+            console.log(response['data']);
+            return response.data;
+          });
+          return promise;
+           
     }
+    
     
   };
   return searchService;
 });
 
-app.controller('ListCtrl', function($scope) {
-    
-});
+
 app.controller('BooksearchCtrl', function($scope, searchService) {
     
     const search = searchService;
-    search.setToken('qqcvhrm19752modk')
+    search.setToken('qqcvhrm19752modk');
     $scope.searchFunc = function() {
         search.getBooks($scope.searchText).then( (d) => $scope.books = d['nyplAPI']['response']['result']) ;
     }
 
 });
-app.controller('BookCtrl', function($scope) {
+app.controller('BookCtrl', function($scope, $routeParams, searchService) {
+    const bookSearch = searchService;
+    bookSearch.setToken('qqcvhrm19752modk');
+    bookSearch.getBookInfo($routeParams.postId).then ( function(data) {
+        $scope.book = data['nyplAPI']['response'];
+    })
     
 })
--- proxy.py
+++ proxy.py
@@ -7,16 +7,6 @@ PORT = 8080
 
     
 class myHandler(BaseHTTPRequestHandler):
-    def byteify(input):
-        if isinstance(input, dict):
-            return {byteify(key): byteify(value)
-                    for key, value in input.iteritems()}
-        elif isinstance(input, list):
-            return [byteify(element) for element in input]
-        elif isinstance(input, unicode):
-            return input.encode('utf-8')
-        else:
-            return input
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', 'http://localhost:3000')
@@ -24,12 +14,12 @@ class myHandler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
         self.send_header("Access-Control-Allow-Headers", "Authorization, Content-Type")
         self.end_headers()
-    def do_GET(self):
+    def do_GET(self): 
         print self.path
         URL = "http://api.repo.nypl.org"
         params = parse_qs(urlparse(self.path).query)
         path = urlparse(self.path).path
-        if params :
+        try :
             trick_headers = {}
             for name, value in self.headers.items():
                 if (name == "authorization"):
@@ -48,8 +38,8 @@ class myHandler(BaseHTTPRequestHandler):
                 self.wfile.write(r.text)
             except:
                 print "something went wrong"
-        else:
-            print "error. please type any param"
+        except:
+            print "error"
         return
 
 
